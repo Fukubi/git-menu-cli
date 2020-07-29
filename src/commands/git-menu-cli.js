5b1a0483373355e13f0ddf2524119b9e9c17c4c1
deivid procopio <undefined>
2020-07-29 10:55:30 -0300

    Diretório do print modificado

--- src/commands/git-menu-cli.js
+++ src/commands/git-menu-cli.js
@@ -1,7 +1,7 @@
 const command = {
   name: 'git-menu-cli',
   run: async toolbox => {
-    await toolbox.menu.showMenu('',{ showHelp: false });
+    await toolbox.menu.showMenu('',{ showHelp: false, headline: 'Selecione o comando a ser executado' });
   }
 }
 
